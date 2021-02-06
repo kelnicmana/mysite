28efce6f5c8482217522d80e7ed8db7ef3c02c1e
Kel Manasco <kellie.manasco@gmail.com>
2021-02-05 22:16:13 -0500

    fixes highlight borders on nav bar items not matching new colors

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
+const navBorder = document.querySelector('#navBorder');
 
 burger.addEventListener('click', () => {
     if (menu.classList.contains('hidden')) {
@@ -8,5 +9,12 @@ burger.addEventListener('click', () => {
     
     else {
             menu.classList.add('hidden');
+    }
+    if (navBorder.classList.contains('border-b')) {
+        navBorder.classList.remove('border-b');
+    }
+    
+    else {
+            navBorder.classList.add('border-b');
     }
 })
